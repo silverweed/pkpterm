3b1779d12aae565bab0f485aee68c61f94e62103
silverweed <silverweed1991@gmail.com>
2015-05-13 16:29:38 +0200

    added comment

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 using Pokepon::Socket;
 
 int main() {
+	// Create a socket and connect it to pokepon.center:12344
 	Socket s;
 	while (s.connected()) {
 		std::cout << "line: " << s.readLine() << std::endl;
